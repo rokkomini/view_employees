a1961d52364704f9a5c40897002c35927962f9ca
Mika Hägerström <mikahagerstrom@hotmail.com>
2022-09-28 12:30:36 +0200

    started structuring and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import React, {useState} from 'react';
-import './App.css';
-import EmployeeItem from './employee-item'
-import axios from 'axios'
-
- 
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import EmployeeItem from "./employee-item";
+import axios from "axios";
 
 function App() {
-  const URI = process.env.REACT_APP_EMPLOYEES_API || 'https://reqres.in/api/users'
+  const [employees, setEmployees] = useState<EmployeeItem[]>([]);
+  const [error, setError] = useState<string | undefined>();
 
-const [employees, setEmployees] = useState<EmployeeItem[]>([])
-const [error, setError] = useState<string | undefined>()
+  const URI =
+    process.env.REACT_APP_EMPLOYEES_API || "https://reqres.in/api/users";
 
-axios.get(URI)
-  .then(response => setEmployees(response.data.data))
-  .catch(err => {
-    setEmployees([]);
-    setError('Somethin went wrong');
-  })
-  
+  function fetchEmployees() {
+    axios
+      .get(URI)
+      .then((response) => setEmployees(response.data.data))
+      .catch((err) => {
+        setEmployees([]);
+        setError("Somethin went wrong");
+      });
+  }
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-      {employees.map(employee => 
-   
-        <p key={employee.id}>{employee.email}</p>
-      
-      )}
-      
-      </header>
+    <div>
+      <div className="container">
+        <h1>Our People</h1>
+        <div className="employee-section">
+          {employees.map((employee) => (
+            <div key={employee.id} className="card">
+              <img src={employee.avatar} alt={`${employee.first_name} ${employee.last_name}`} />
+              <p>{employee.first_name} {employee.last_name}</p>
+              <p >{employee.email}</p>
+            </div>
+          ))}
+        </div>
+        
+      </div>
+      <div className="pagination">pages</div>
     </div>
   );
 }
